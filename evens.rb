f255d49b4fef2d37f3415e1b1b03a023614345e4
James Dabbs <jamesdabbs@gmail.com>
2017-01-09 14:28:09 -0500

    Guard against invalid input

--- evens.rb
+++ evens.rb
@@ -1,28 +1,32 @@
 # require 'pry'
 
-print 'Enter a number (or "quit" to exit) > '
-input = gets.chomp
+words_that_quit = ["quit", "q", "exit", "stop", "help"]
 
-while input != "quit"
-  begin
-    number = Integer(input)
-
-    # raise 'Forced raise'
-
-    if number.even?
-      puts "#{number} is even"
-    else
-      puts "#{number} is odd"
-    end
-
-  rescue ArgumentError => e
-    puts "\"#{input}\" is not a number"
-  rescue RuntimeError => e
-    puts "Other error: #{e}"
-  end
-
+loop do
   print 'Enter a number (or "quit" to exit) > '
   input = gets.chomp
+
+  if words_that_quit.include?(input.downcase)
+    break
+  end
+
+  number = nil
+  loop do
+    begin
+      number = Integer(input)
+      break # out of this loop; I now have a `number`
+    rescue
+      puts "\"#{input}\" is not a number"
+      print 'Try again > '
+      input = gets.chomp
+    end
+  end
+
+  if number.even?
+    puts "#{number} is even"
+  else
+    puts "#{number} is odd"
+  end
 end
 
 puts "We're done"
