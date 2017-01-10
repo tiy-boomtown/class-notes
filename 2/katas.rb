925bea8f68fa84fa291276141224906ecb252dfa
James Dabbs <jamesdabbs@gmail.com>
2017-01-10 09:24:47 -0500

    Solutions to first kata

---
+++ 2/katas.rb
@@ -0,0 +1,22 @@
+# 1. Print the numbers 1-10 using `loop`
+
+# number = 0
+# while number < 10
+#   number += 1
+#   puts number
+# end
+
+# number = 1
+# loop do
+#   puts number
+#   number += 1
+#   if number > 10
+#     break
+#   end
+# end
+
+# 2. Print the numbers 1-10 using `each`
+
+___.each do |number|
+  
+end
