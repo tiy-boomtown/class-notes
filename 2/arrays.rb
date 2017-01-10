ccce51a242ade9e98230cf2e16ac69958e853d57
James Dabbs <jamesdabbs@gmail.com>
2017-01-10 11:36:50 -0500

    Add sum

--- 2/arrays.rb
+++ 2/arrays.rb
@@ -29,5 +29,24 @@ numbers.each do |n|
   if n.odd?
     odds.push n
   end
+  # odds.push n if n.odd?
 end
 print_numbers odds
+
+puts "5s" # 15, 25, 53, ...
+fives = []
+numbers.each do |n|
+  # fives.push n if n.to_s.include? "5"
+  if n.to_s.include? "5"
+    fives.push n
+  end
+end
+print_numbers fives
+
+puts "Sum"
+sum = 0
+numbers.each do |n|
+  # sum += n ~> sum = sum + n
+  sum = n + sum
+end
+puts sum
