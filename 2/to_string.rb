788d9c65e19f0a4d2b942415217d855921519193
James Dabbs <jamesdabbs@gmail.com>
2017-01-11 08:57:42 -0500

    Add notes on `to_s` and interpolation

---
+++ 2/to_string.rb
@@ -0,0 +1,36 @@
+arr = [1,2,3]
+
+puts 'With puts'
+
+puts 2
+
+hash = { a: 1, b: 2 }
+puts hash
+
+puts 'arr'
+puts arr
+puts
+
+puts '"#{arr}"'
+puts "#{arr}"
+puts
+
+puts 'arr.to_s'
+puts arr.to_s
+puts
+
+
+puts 'With print:'
+
+puts 'arr'
+print arr
+puts
+
+puts '"#{arr}"'
+print "#{arr}"
+puts
+
+puts 'arr.to_s'
+print arr.to_s
+puts
+
