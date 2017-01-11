842085588a6812c67858e9a7c23ae3143529a3ab
James Dabbs <jamesdabbs@gmail.com>
2017-01-11 11:56:25 -0500

    First class definition

--- 3/classes.rb
+++ 3/classes.rb
@@ -1,7 +1,7 @@
 class Counter
-  def initialize
+  def initialize(n)
     # puts "init"
-    @number = 0
+    @number = n
   end
 
   def up
@@ -15,22 +15,31 @@ class Counter
     @number
   end
 
-  def reset
-    @number = 0
+  # def reset(n=nil)
+  #   if n == nil
+  #     @number = 0
+  #   else
+  #     @number = n
+  #   end
+  # end
+  def reset(n=0)
+    @number = n
   end
 end
 
-c = Counter.new
-d = Counter.new
+c = Counter.new 5
+d = Counter.new 10
 
 c.up
 d.up
 c.up
 
-puts c.count # 2
+puts c.count # 7
+puts d.count # ...
+
+c.reset 20
+puts c.count # 20
 puts d.count # 1
 
 c.reset
-
 puts c.count # 0
-puts d.count # 1
