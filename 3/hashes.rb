816e4fd339f836bb1c0893d820512402fd45babe
James Dabbs <jamesdabbs@gmail.com>
2017-01-11 10:20:27 -0500

    First pass at letter distribution

--- 3/hashes.rb
+++ 3/hashes.rb
@@ -9,9 +9,31 @@ def letter_count(str, letter)
   str.split('').select { |c| c == letter }.count
 end
 
+def letter_frequencies(str)
+  # {
+  #     a: letter_count(str, 'a'),
+  #     b: letter_count(str, 'b'),
+  #     c: letter_count(str, 'c')
+  # }
+
+  result = {}
+  # result['a'] = letter_count(str, 'a')
+  # result['b'] = letter_count(str, 'b')
+  ('a' .. 'z').each do |letter|
+    result[ letter ] = letter_count(str, letter)
+  end
+  result
+end
+
+# ----
+
 str = "where's the beef?"
 
 n = letter_count(str, 'e')
 puts "#{n} e's"
 
 puts "#{letter_count(str, 'a')} a's"
+
+freq = letter_frequencies(str)
+
+puts "There are #{freq['e']} many e's"
