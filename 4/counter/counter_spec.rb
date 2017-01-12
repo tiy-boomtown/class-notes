d2037a5caf2bebe75754ea382a9c1f4a7c6775cf
James Dabbs <jamesdabbs@gmail.com>
2017-01-12 10:41:08 -0500

    Extract counter class to new file

--- 4/counter/counter_spec.rb
+++ 4/counter/counter_spec.rb
@@ -1,22 +1,5 @@
 require 'rspec'
-
-class Counter
-  def initialize(start=0)
-    @n = start
-  end
-
-  def up
-    @n += 1
-  end
-
-  def count
-    @n
-  end
-
-  def reset
-    @n = 0
-  end
-end
+require_relative 'counter'
 
 describe Counter do
   it 'increments' do
