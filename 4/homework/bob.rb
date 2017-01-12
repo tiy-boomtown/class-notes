2509b11265c7e82646ede5bdbcfdbd205563c440
James Dabbs <jamesdabbs@gmail.com>
2017-01-12 14:24:11 -0500

    Passing Bob specs

--- 4/homework/bob.rb
+++ 4/homework/bob.rb
@@ -1,2 +1,18 @@
 class Bob
+  def hey(msg)
+    if is_angry? msg
+      'Whoa, chill out!'
+    elsif msg.end_with? '?'
+      'Sure.'
+    elsif msg.strip.empty?
+      'Fine. Be that way!'
+    else
+      'Whatever.'
+    end
+  end
+
+  def is_angry? msg
+    return false unless ('A' .. 'Z').any? { |l| msg.include? l }
+    msg == msg.upcase
+  end
 end
--- 4/homework/bob_spec.rb
+++ 4/homework/bob_spec.rb
@@ -2,104 +2,104 @@ require 'rspec'
 require_relative 'bob'
 
 describe Bob do
-  let(:bob) { ::Bob.new }
+  let(:bob) { Bob.new }
   
   it 'can state something' do
     remark = 'Tom-ay-to, tom-aaaah-to.'
     expect(bob.hey remark).to eq 'Whatever.'
   end
 
-  skip 'can shout' do
+  it 'can shout' do
     remark = 'WATCH OUT!'
     expect(bob.hey remark).to eq 'Whoa, chill out!'
   end
 
-  skip 'can shout gibberish' do
+  it 'can shout gibberish' do
     remark = ('A'..'Z').to_a.shuffle[0, 10].join
     expect(bob.hey remark).to eq 'Whoa, chill out!'
   end
 
-  skip 'can ask a question' do
+  it 'can ask a question' do
     remark = 'Does this cryogenic chamber make me look fat?'
     expect(bob.hey remark).to eq 'Sure.'
   end
 
-  skip 'can ask a numeric question' do
+  it 'can ask a numeric question' do
     remark = 'You are, what, like 15?'
     expect(bob.hey remark).to eq 'Sure.'
   end
 
-  skip 'can ask gibberish' do
+  it 'can ask gibberish' do
     remark = ('a'..'z').to_a.shuffle[0, 10].join << '?'
     expect(bob.hey remark).to eq 'Sure.'
   end
 
-  skip 'can talk forcefully' do
+  it 'can talk forcefully' do
     remark = "Let's go make out behind the gym!"
     expect(bob.hey remark).to eq 'Whatever.'
   end
 
-  skip 'can use acronyms in regular speech' do
+  it 'can use acronyms in regular speech' do
     remark = "It's OK if you don't want to go to the DMV."
     expect(bob.hey remark).to eq 'Whatever.'
   end
 
-  skip 'can ask forceful questions' do
+  it 'can ask forceful questions' do
     remark = 'WHAT THE HELL WERE YOU THINKING?'
     expect(bob.hey remark).to eq 'Whoa, chill out!'
   end
 
-  skip 'can shout numbers' do
+  it 'can shout numbers' do
     remark = '1, 2, 3 GO!'
     expect(bob.hey remark).to eq 'Whoa, chill out!'
   end
 
-  skip 'can say only numbers' do
+  it 'can say only numbers' do
     remark = '1, 2, 3'
     expect(bob.hey remark).to eq 'Whatever.'
   end
 
-  skip 'can question with only numbers' do
+  it 'can question with only numbers' do
     remark = '4?'
     expect(bob.hey remark).to eq 'Sure.'
   end
 
-  skip 'can shout with special characters' do
+  it 'can shout with special characters' do
     remark = 'ZOMG THE %^*@#$(*^ ZOMBIES ARE COMING!!11!!1!'
     expect(bob.hey remark).to eq 'Whoa, chill out!'
   end
 
-  skip 'can shout with no exclamation mark' do
+  it 'can shout with no exclamation mark' do
     remark = 'I HATE YOU'
     expect(bob.hey remark).to eq 'Whoa, chill out!'
   end
 
-  skip 'can make a statement containing a question mark' do
+  it 'can make a statement containing a question mark' do
     remark = 'Ending with ? means a question.'
     expect(bob.hey remark).to eq 'Whatever.'
   end
 
-  skip 'can prattle on' do
+  it 'can prattle on' do
     remark = 'Wait! Hang on. Are you going to be OK?'
     expect(bob.hey remark).to eq 'Sure.'
   end
 
-  skip 'can be silent' do
+  it 'can be silent' do
     remark = ''
     expect(bob.hey remark).to eq 'Fine. Be that way!'
   end
 
-  skip 'can be really silent' do
+  it 'can be really silent' do
     remark = ' ' * rand(1..10)
     expect(bob.hey remark).to eq 'Fine. Be that way!'
   end
 
-  skip 'can alternate silences' do
+  it 'can alternate silences' do
     remark = "\t" * rand(1..10)
     expect(bob.hey remark).to eq 'Fine. Be that way!'
   end
 
-  skip 'can handle multiple line questions' do
+  it 'can handle multiple line questions' do
     remark = %(
 Does this cryogenic chamber make me look fat?
 no)
