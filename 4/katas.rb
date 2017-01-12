8f19064bd32a388b6a3cf701c6c4d3268e29c7b3
James Dabbs <jamesdabbs@gmail.com>
2017-01-12 09:27:14 -0500

    Add solutions, person kata

--- 4/katas.rb
+++ 4/katas.rb
@@ -3,3 +3,27 @@
 
 words = File.readlines '/usr/share/dict/words'
 puts words.count # => 235886 on my machine
+
+x_words = []
+words.each do |word|
+  # if word[-2] == 'x'
+  if word.chomp.end_with? 'x'
+    x_words.push word
+  end
+end
+puts x_words.count
+
+x_words = words.select do |word|
+  word.chomp.end_with? 'x'
+end
+x_words = words.select { |w| w.chomp.end_with? 'x' }
+puts x_words.count
+
+# 2. Class practice
+
+class Person
+  # Fill this out!
+end
+
+p = Person.new 'Your', 'Name'
+puts p.greet # => 'Hi, my name is Your Name'
