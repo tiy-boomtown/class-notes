a1914037c815883a444c5247b6ca1639ce1dc445
James Dabbs <jamesdabbs@gmail.com>
2017-01-12 10:55:43 -0500

    Add acceptance test

--- 4/fizzbuzz/fizzbuzzer_spec.rb
+++ 4/fizzbuzz/fizzbuzzer_spec.rb
@@ -5,4 +5,38 @@ describe Fizzbuzzer do
   it 'works' do
     expect(1 + 1).to eq 2
   end
+
+  skip 'really works' do
+    f = Fizzbuzzer.new
+
+    f.register_rule(4, 'Cat')
+    f.register_rule(5, 'Dog')
+
+    result = [
+        1,
+        2,
+        'Cat',
+        'Dog',
+        6,
+        7,
+        'Cat',
+        9,
+        'Dog',
+        11,
+        'Cat',
+        13,
+        14,
+        'Dog',
+        'Cat',
+        17,
+        18,
+        19,
+        'CatDog',
+        21,
+        22
+    ]
+    expect(f.run 22).to eq result
+  end
+
+
 end
