9fed282dd45adc9a949ef1788722e664b2927b3a
James Dabbs <jamesdabbs@gmail.com>
2017-01-13 13:44:03 -0500

    Start on first `board_full?` test

--- 5/tictactoe/xos.rb
+++ 5/tictactoe/xos.rb
@@ -1,6 +1,9 @@
 class XOs
+  attr_reader :turns_left
+
   def initialize
     @turns_left = 9
+    @board = []
   end
 
   def winner
@@ -10,7 +13,12 @@ class XOs
     @turns_left == 0
   end
 
-  def take_turn
+  def take_turn(letter, position)
     @turns_left -= 1
+    @board[position - 1] = letter
+  end
+
+  def letter_at(position)
+    @board[position - 1]
   end
 end
--- 5/tictactoe/xos_spec.rb
+++ 5/tictactoe/xos_spec.rb
@@ -2,6 +2,20 @@ require 'rspec'
 require_relative 'xos'
 
 describe XOs do
+  it 'can take one turn' do
+    x = XOs.new
+
+    # I take one turn ...
+    # where I place an X at 3
+    x.take_turn('X', 3)
+
+    # Something happens ...
+    expect(x.turns_left).to eq 8
+
+    # I see the X on the board (at 3?)
+    expect(x.letter_at 3).to eq 'X'
+  end
+
   skip 'can fill up the board' do
     x = XOs.new
 
