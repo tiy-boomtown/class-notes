9688e73b83c51dd3b25fc1d95e421d0552015559
James Dabbs <jamesdabbs@gmail.com>
2017-01-13 13:25:36 -0500

    Start on first `board_full?` test

--- 5/tictactoe/xos.rb
+++ 5/tictactoe/xos.rb
@@ -1,7 +1,16 @@
 class XOs
+  def initialize
+    @turns_left = 9
+  end
+
   def winner
   end
 
   def board_full?
+    @turns_left == 0
+  end
+
+  def take_turn
+    @turns_left -= 1
   end
 end
--- 5/tictactoe/xos_spec.rb
+++ 5/tictactoe/xos_spec.rb
@@ -2,5 +2,14 @@ require 'rspec'
 require_relative 'xos'
 
 describe XOs do
-  it 'can fill up the board'
+  skip 'can fill up the board' do
+    x = XOs.new
+
+    # I fill up the board
+    9.times do
+      x.take_turn
+    end
+
+    expect(x.board_full?).to eq true
+  end
 end
