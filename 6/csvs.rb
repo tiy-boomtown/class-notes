1b4fe089974045330aa7e3c9d58c2eeac7985e8e
James Dabbs <jamesdabbs@gmail.com>
2017-01-16 14:17:12 -0500

    Writing a CSV

--- 6/csvs.rb
+++ 6/csvs.rb
@@ -14,3 +14,12 @@ end
 
 puts "Max price is #{prices.max}"
 puts "Total revenue is #{revenue}"
+
+CSV.open('/Users/james/Code/class-notes/6/output.csv', 'w') do |csv|
+  csv << ['Max Price', 'Total Revenue', 'Some Commas']
+  csv << [prices.max, revenue, 'a string, with, commas']
+
+  (1 .. 10).each do |n|
+    csv << [n, n*2, n*3]
+  end
+end
