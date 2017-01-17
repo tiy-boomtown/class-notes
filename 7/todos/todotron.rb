e0ad5b235a678782386624708263ba0deae65d06
James Dabbs <jamesdabbs@gmail.com>
2017-01-17 11:56:01 -0500

    Can add items to list

--- 7/todos/todotron.rb
+++ 7/todos/todotron.rb
@@ -1,2 +1,11 @@
 class TodoTron
+  attr_reader :items
+
+  def initialize
+    @items = []
+  end
+
+  def add_item(description)
+    @items.push description
+  end
 end
--- 7/todos/todotron_spec.rb
+++ 7/todos/todotron_spec.rb
@@ -1,6 +1,18 @@
 require 'rspec'
+require_relative 'todo'
 require_relative 'todotron'
 
 describe TodoTron do
-  # ...
+  it 'can track todos' do
+    tron = TodoTron.new
+
+    # It has no todo items
+    expect(tron.items.count).to eq 0
+
+    # I add an item
+    tron.add_item 'Buy groceries'
+
+    # It has one item
+    expect(tron.items.count).to eq 1
+  end
 end
